10773305a18c29cf0453360f4124506fcc267102
Ivan Razbegaev <sinnless13+1@gmail.com>
2021-10-20 14:24:06 +0300

    table added

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const incidentsTable = document.getElementById("customers");
+const nodeOneBtn = document.getElementById("node-one");
+const nodeTwoBtn = document.getElementById("node-two");
+const nodeThreeBtn = document.getElementById("node-three");
+const allNodesBtn = document.getElementById("all-nodes");
 
 const incident ={
     dataArray: {},
@@ -8,10 +13,6 @@ const incident ={
     incidents: [],
 
     init: function () {
-        const nodeOneBtn = document.getElementById("node-one");
-        const nodeTwoBtn = document.getElementById("node-two");
-        const nodeThreeBtn = document.getElementById("node-three");
-        const allNodesBtn = document.getElementById("all-nodes");
 
         nodeOneBtn.addEventListener('click',  () => {
             this.start(1);
@@ -29,6 +30,8 @@ const incident ={
 
     start: function (node = 'all') {
         this.nodeIncidents(node);
+        this.renderTable();
+        this.incidents =[];
     },
 
     getInfluxShit: function (query){
@@ -36,10 +39,13 @@ const incident ={
         fetch(query).then( (response) => {
             return response.json();
         }).then((data) => {
-            this.dataArray = data.results[0].series[0];
-            this.getIncidents();
-        }).catch(() => {
-            console.log("Something went wrong. Use console.dir to look for result of request");
+            // this.dataArray = data.results[0].series[0];
+            let newArray = JSON.stringify(data.results[0].series[0].values);
+            console.log(newArray);
+            setTimeout(() => this.getIncidents(),0);
+            this.logger()
+        }).catch((e) => {
+            console.log("Something went wrong. Use console.dir to look for result of request", e);
         });
     },
 
@@ -47,40 +53,49 @@ const incident ={
         const incidentTime = 0.15;
         for (let i = 0; i < this.dataArray.values.length; i++){
             if (+this.dataArray.values[i][2] > +incidentTime){
-                this.incidents.push({time: this.dataArray.values[i][0],
+                this.incidents.push({
+                    time: this.dataArray.values[i][0],
                     host: this.dataArray.values[i][1],
                     resp_time:this.dataArray.values[i][2],
                     pinup: false
                 });
-            } else continue;
+            }
         }
-        console.dir(incident.incidents);
     },
     nodeIncidents: function (node = "all") {
         switch (true) {
             case node == 1:
                 this.getInfluxShit(this.url+this.influxQueryNode1);
-                this.incidents = [];
                 break;
             case node == 2:
                 this.getInfluxShit(this.url+this.influxQueryNode2);
-                this.incidents = [];
                 break;
             case node == 3:
                 this.getInfluxShit(this.url+this.influxQueryNode3);
-                this.incidents = [];
                 break;
             default:
                 this.getInfluxShit(this.url+this.influxQueryNode1);
                 this.getInfluxShit(this.url+this.influxQueryNode2);
                 this.getInfluxShit(this.url+this.influxQueryNode3);
-                this.incidents = [];
+                break;
         }
     },
-    showResults: function (){
+    logger: function (){
+        console.log(this.incidents)
+    },
 
-    }
-
+    renderTable: function () {
+      //TODO Создание таблицы на основе HTML элементов
+        this.incidents.forEach(value => {
+            let tr = document.createElement("tr");
+            tr.innerHTML =
+            '<td>' + value.time.toLocaleString("ru") + '</td>' +
+            '<td>' + value.host + '</td>' +
+            '<td>' + value.resp_time.toFixed(2) + '</td>' +
+            '<td>' + value.pinup.toString() + '</td>';
+            incidentsTable.append(tr);
+        })
+    },
 }
 
 incident.init();
